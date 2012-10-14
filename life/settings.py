7db475447a642e816f4baa7bb8be0f7b7ba9011f
warrenwjackson <wj@warrenjackson.com>
2012-10-13 18:47:55 -0700

    Added heroku db settings

--- life/settings.py
+++ life/settings.py
@@ -1,5 +1,5 @@
 # Django settings for life project.
-
+import dj_database_url
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -8,17 +8,20 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'life_db',                      # Or path to database file if using sqlite3.
-        'USER': 'life_db_user',                      # Not used with sqlite3.
-        'PASSWORD': '123',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+#DATABASES = {
+#             
+#
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'life_db',                      # Or path to database file if using sqlite3.
+#        'USER': 'life_db_user',                      # Not used with sqlite3.
+#        'PASSWORD': '123',                  # Not used with sqlite3.
+#        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
